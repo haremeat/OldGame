84c35a9e087992a845625bfcad62bd0c4b39cd48
haremeat <dmswjd9341@naver.com>
2021-07-16 15:49:44 +0900

    character movement

--- 1_create_frame.py
+++ 1_create_frame.py
@@ -21,6 +21,10 @@ character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2) # 화면 중앙에 위치
 character_y_pos = (screen_height - character_height) # 가장 아래 위치
 
+# 이동할 좌표
+to_x = 0
+to_y = 0
+
 # 이벤트 루프
 running = True # 게임 진행중인가?
 while running:
@@ -28,6 +32,25 @@ while running:
         if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
             running = False # 게임이 진행중이 아님
 
+        if event.type == pygame.KEYDOWN: # 키가 눌러졌는지 확인
+            if event.key == pygame.K_LEFT:
+                to_x -= 1
+            elif event.key == pygame.K_RIGHT:
+                to_x += 1
+            elif event.key == pygame.K_UP:
+                to_y -= 1
+            elif event.key == pygame.K_DOWN:
+                to_y += 1
+
+        if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+            if event.type == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.type == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+    # 캐릭터 좌표값 이동
+    character_x_pos += to_x
+    character_y_pos += to_y
 
     #screen.fill((0,0,255))
     screen.blit(background, (0, 0)) # 배경 그리기
